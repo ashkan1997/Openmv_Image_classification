81d597423ae146bc7986a98cf2da9ef44ed36b7e
ashkan1997 <ashkan.behmanesh@studenti.unitn.it>
2023-05-20 01:43:06 +0200

    OpenmvV1

---
+++ openmvV1.py
@@ -0,0 +1,57 @@
+import sensor, image, pyb, os, time, tf, uos, gc
+
+TRIGGER_THRESHOLD = 5
+BG_UPDATE_FRAMES = 50
+BG_UPDATE_BLEND = 128
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time=2000)
+sensor.set_auto_whitebal(False)
+clock = time.clock()
+
+extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.RGB565)
+
+net = None
+labels = None
+
+try:
+    net = tf.load("model.tflite", load_to_fb=uos.stat('model.tflite')[6] > (gc.mem_free() - (32*1024)))
+except Exception as e:
+    print(e)
+    raise Exception('Failed to load "model.tflite", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+try:
+    labels = [line.rstrip('\n') for line in open("labels.txt")]
+except Exception as e:
+    raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+frame_count = 0
+
+while True:
+    clock.tick()
+    img = sensor.snapshot()
+
+    frame_count += 1
+    if frame_count > BG_UPDATE_FRAMES:
+        frame_count = 0
+        img.blend(extra_fb, alpha=(256-BG_UPDATE_BLEND))
+        extra_fb.replace(img)
+
+    img.difference(extra_fb)
+
+    hist = img.get_histogram()
+    diff = hist.get_percentile(0.99).l_value() - hist.get_percentile(0.90).l_value()
+    triggered = diff > TRIGGER_THRESHOLD
+
+    if triggered:
+        for obj in net.classify(img, min_scale=1.0, scale_mul=0.8, x_overlap=0.5, y_overlap=0.5):
+            print("**********\nPredictions at [x=%d,y=%d,w=%d,h=%d]" % obj.rect())
+            img.draw_rectangle(obj.rect())
+            predictions_list = list(zip(labels, obj.output()))
+
+            for i in range(len(predictions_list)):
+                print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+
+    print(clock.fps(), triggered)
